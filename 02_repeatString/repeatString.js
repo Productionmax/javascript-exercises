eacd62e5e3af6cae2694e2e66d40daafea268c05
Jonathan Killen <Productionmax@hotmail.com>
2022-07-27 11:16:25 +0800

    Finished reverse string activity 03 and learned how to reverse a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+let repeatedString = "";
+const repeatString = function(str,times) {
+    for (let i=0;i<times;i++){
+        if (times < 0){
+            break;
+        }
+        else{repeatedString += str};
+    }
+    return repeatedString;  
 };
-
+let times=0;
+let str = "hey";
 // Do not edit below this line
 module.exports = repeatString;
