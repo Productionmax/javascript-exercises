e7e5a838d0dcb549862ab141860d9cdb964de270
Jonathan Killen <Productionmax@hotmail.com>
2022-07-27 21:36:03 +0800

    completed function to remove multiple values within an array and learned rest parameters and (indexof) and(includes) feature

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (Array, ...multArgs) {
+    // let removedValue1 = Array.indexOf(multArgs);
+    // console.log(multArgs);
+    // let removed = Array.splice(removedValue1, 1);
+    //console.log(removed);
+    // return Array;
+    for (let i = 0; i < Array.length; i++){
+        if (Array.includes(multArgs[i])) {
+                Array.splice(Array.indexOf(multArgs[i]), 1)
+        }
+    } 
+    return Array;
 };
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//npm test removeFromArray.spec.js
