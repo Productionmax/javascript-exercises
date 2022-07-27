3891dd1040ba67c2e82b1c7237c0b826c66c5087
Jonathan Killen <Productionmax@hotmail.com>
2022-07-27 22:31:52 +0800

    Finished removedFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,18 @@
-const removeFromArray = function (Array, ...multArgs) {
+const removeFromArray = function (presentArray, ...multArgs) {
     // let removedValue1 = Array.indexOf(multArgs);
     // console.log(multArgs);
     // let removed = Array.splice(removedValue1, 1);
     //console.log(removed);
     // return Array;
-    for (let i = 0; i < Array.length; i++){
+    let Array = presentArray.slice(); // need to do this so that the present array stays fresh so that iterations dont run out
+    for (let i = 0; i < presentArray.length; i++){ // so the unchanging array is the one used in comparison
         if (Array.includes(multArgs[i])) {
                 Array.splice(Array.indexOf(multArgs[i]), 1)
-        }
+        } 
     } 
     return Array;
 };
-console.log(removeFromArray([1, 2, 3, 4], 3, 2));
+console.log(removeFromArray([1, 2, 3], "1", 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
