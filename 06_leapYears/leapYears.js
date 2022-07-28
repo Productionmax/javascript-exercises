bca36d2145adfa75fd6c901430c79a6606bc3d0f
Jonathan Killen <Productionmax@hotmail.com>
2022-07-28 17:05:47 +0800

    Finished the leapyears exercise and specifically trained my skills on IF statements and the && notation

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if (years % 4 === 0 && years % 100 > 0){
+        return true;
+    } else if (years % 400 === 0){
+        return true;
+    }
+    else {return false};
 };
-
+console.log(leapYears(700));
 // Do not edit below this line
 module.exports = leapYears;
+
+// years % 4 = 0 => leap years
+// years % 100 = 0 && !(years % 400 = 0) => not leap years
+// npm test leapYears.spec.js
