950a65ec782b24fc2890e4aa60943894d602f7c1
Jonathan Killen <Productionmax@hotmail.com>
2022-07-28 16:12:59 +0800

    Finished sumAll exercise and i learned how to choose the maximum and minimum number within a range in order to use a for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstNum,secondNum) {
+    let range = [firstNum,secondNum];
+    let max = Math.max(firstNum,secondNum); // FInds the largest value within the list
+    let min = Math.min(firstNum,secondNum); // FInds the smallest value within the list
+    let totalSum = 0;
+    if (min>0 && max>0 && typeof firstNum === 'number' && typeof secondNum === 'number'){
+        for (i=min;i<=max;i++){
+        totalSum += i;
 
-};
-
+        }
+    } else{
+        totalSum = 'ERROR'
+    } 
+    
+    return totalSum;
+};  
+console.log(sumAll(123,4))
 // Do not edit below this line
 module.exports = sumAll;
+// npm test sumAll.spec.js
