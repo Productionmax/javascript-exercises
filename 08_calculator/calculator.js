b911d913a6aa04b12065623b29a8493a356bc64d
Jonathan Killen <Productionmax@hotmail.com>
2022-07-30 10:35:38 +0800

    Successfully completed the calculator exercise and major learning point of this exercise was the ability to do factorials in javascirpt and multiplication in array

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,49 @@
-const add = function() {
-	
+const add = function(add1,add2) {
+  let addition = add1 + add2;
+  return addition;
 };
 
-const subtract = function() {
-	
+const subtract = function(sub1,sub2) {
+	let subtraction = sub1 - sub2;
+  return subtraction;
 };
 
-const sum = function() {
-	
+const sum = function(sumArr) {
+	let total = 0;
+  for (let i=0;i<sumArr.length;i++){
+    total += sumArr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(multiplyArr) {
+  let product = 1;
+  for (let i=0;i<multiplyArr.length;i++){
+    product = product * multiplyArr[i];
+  } 
+  return product;
 };
 
-const power = function() {
-	
+const power = function(lower,indices) {
+	let exponential = lower ** indices;
+  return exponential;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num ===1){
+    return 1;
+  } 
+  for (let i=num-1;i>=1;i--){
+    num = num * i;
+  }
+  return num;
 };
-
+console.log(add(2,6));
+console.log(subtract(10,4));
+console.log(sum([1,3,5,7,9]));
+console.log(multiply([2,4,6,8,10,12,14]));
+console.log(power(4,3));
+console.log(factorial(10));
 // Do not edit below this line
 module.exports = {
   add,
@@ -31,3 +53,4 @@ module.exports = {
   power,
   factorial
 };
+// npm test calculator.spec.js
