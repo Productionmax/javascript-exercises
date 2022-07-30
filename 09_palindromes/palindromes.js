03e92f77c76a59b80ad165d98ab03a437c5b5bde
Jonathan Killen <Productionmax@hotmail.com>
2022-07-30 16:03:55 +0800

    Palindromes exercise completed. In this exercise I learned how to remove punctuation by xxx.replace function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (palin) {
+    let stringClean = palin.replace(/\W/g, '');
+    let StringLower = stringClean.toLowerCase();
+    let splitString = StringLower.split("");
+    let reverse = splitString.reverse();
+    let joinArray = reverse.join("");
+    if (joinArray === StringLower){
+        return true;
+    } else {
+        return false;
+    }
 };
-
+console.log(palindromes("ZZZZ car, a man, a maracaz."));
 // Do not edit below this line
 module.exports = palindromes;
+// npm test palindromes.spec.js
