c21cadb4e8e39524e5268893a1989dcb3ded0c6e
Jonathan Killen <Productionmax@hotmail.com>
2022-08-04 18:13:41 +0800

    findtheOldest activity still very confused

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,29 +1,35 @@
 const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ]
-const findTheOldest = function(people) {
-    return people.sort(function (a,b){
-        const aPeople = a.yearOfDeath - a.yearOfBirth;
-        const bPeople = b.yearOfDeath - a.yearOfBirth;
-        if (aPeople > bPeople){
-            return -1
-        } else {return 1};
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+
+const findTheOldest = function(people){
+    return people.reduce(function (oldest,currentPerson){
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath); //Uses the function to get old age
+        const currentAge = getAge(currentPerson.yearOfBirth, oldest.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
     })
 };
-console.log(people);
+// const getAge = function(a,b){
+//     if(!b){ //If not year of death 
+//         b = new Date().getFullYear(); //the date is the new date to find new age 
+//     }
+//     return b-a;
+// };
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
+//npm test findTheOldest.spec.js
