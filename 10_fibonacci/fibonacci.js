893e110d4c6a3640038fa469931963c0bae48ef8
Jonathan Killen <Productionmax@hotmail.com>
2022-08-04 15:16:27 +0800

    Completed the Fibonacci activity: Learned how to make a fibonacci function with the use of arrays and a for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n<0){
+        return "OOPS";
+    }
+    if (n <= 1){
+        return n;
+    }
+    const result = [1,1];
+    for (let i = 2; i<= n;i++){
+        result[i] = result[i-2] + result [i-1];
+    }
+    return result[n-1];
 };
-
+console.log(fibonacci("2"));
 // Do not edit below this line
 module.exports = fibonacci;
+//npm test fibonacci.spec.js
